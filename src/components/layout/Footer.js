83f42bfa75999162b9cf1d62ceee467a73737aa9
alwaysPasindu <always.pasindu@gmail.com>
2025-02-24 03:36:05 +0530

    fixed some known issues to the footer elements

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,12 +8,12 @@ const Footer = () => {
         {/* Footer Top Section */}
         <div className="footer-top">
           <div className="footer-brand">
-            <a href="#hero" className="logo">We Neighbour</a>
+            <a href="#hero" className="logo">WE NEIGHBOUR</a>
             <p>Connecting neighbors, strengthening communities.</p>
             
             {/* Social Links */}
             <div className="social-links">
-              <a href="https://twitter.com/weneighbour" className="twitter"><i className="bi bi-twitter"></i></a>
+              {/* <a href="https://twitter.com/weneighbour" className="twitter"><i className="bi bi-twitter"></i></a> */}
               <a href="https://www.facebook.com/p/We-Neighbour-61568788907715/" className="facebook"><i className="bi bi-facebook"></i></a>
               <a href="https://www.instagram.com/we_neighbour/profilecard/?igsh=MXFuM2k1MmtpbGh4ZA%3D%3D" className="instagram"><i className="bi bi-instagram"></i></a>
               <a href="https://www.linkedin.com/company/we-neighbour/" className="linkedin"><i className="bi bi-linkedin"></i></a>
@@ -33,15 +33,15 @@ const Footer = () => {
 
           {/* Contact Info */}
           <div className="footer-contact">
-            <p><i className="bi bi-geo-alt"></i> 123 Community Street, San Francisco, CA</p>
-            <p><i className="bi bi-telephone"></i> +1 234 567 8900</p>
-            <p><i className="bi bi-envelope"></i> hello@weneighbour.com</p>
+            <p><i className="bi bi-geo-alt"></i> Colombo, Sri Lanka</p>
+            <p><i className="bi bi-telephone"></i> +94 76 123 4567</p>
+            <p><i className="bi bi-envelope"></i> we.neighbour.live@gmail.com</p>
           </div>
         </div>
 
         {/* Footer Bottom Section */}
         <div className="footer-bottom">
-          <p>&copy;2025 <strong>We Neighbour</strong>. All Rights Reserved.</p>
+          <p>&copy; 2025 <strong>WE NEIGHBOUR</strong>. All Rights Reserved.</p>
         </div>
       </div>
     </footer>
